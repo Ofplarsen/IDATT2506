9fad1a8894557658b2cd506f90273960cd2d5b61
ofplarsen <ofplarsen@gmail.com>
2022-08-22 19:52:45 +0200

    New activity created. Ready to implement intent

---
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/ChangePicActivity.kt
@@ -0,0 +1,11 @@
+package edu.ntnu.oflarsen.idatt2506.oving2
+
+import android.app.Activity
+import android.os.Bundle
+
+class ChangePicActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
