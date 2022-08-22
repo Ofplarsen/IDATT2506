d515bbc8ec535da05a1b59dba6c3c3fcf6c9ec00
ofplarsen <ofplarsen@gmail.com>
2022-08-22 20:27:26 +0200

    Intent implemented. Switches between pictures

--- Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
@@ -1,11 +1,17 @@
 package edu.ntnu.oflarsen.idatt2506.oving2
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClickStartChangePicActivity(v: View?) {
+        val intent = Intent("oving2.ChangePicActivity")
+        startActivity(intent) }
 }
