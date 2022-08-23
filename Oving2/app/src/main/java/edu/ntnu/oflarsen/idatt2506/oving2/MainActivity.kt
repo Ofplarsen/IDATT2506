8c95fe79803c02fa92180c3e9cbfdb5fdf3ed759
ofplarsen <ofplarsen@gmail.com>
2022-08-23 10:59:49 +0200

    Task 1 done

--- Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
@@ -1,11 +1,21 @@
 package edu.ntnu.oflarsen.idatt2506.oving2
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun generateRandomNumber(view: View?){
+        val value = (0..100).random()
+
+        Toast.makeText(this, value.toString(), Toast.LENGTH_LONG).show()
     }
 }
