d6e296b8566c1f9c2a7fa7d82d9b0fa16469db7d
ofplarsen <ofplarsen@gmail.com>
2022-08-23 13:03:34 +0200

    Task 1 almost finished. Still some bugs with random

---
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/GenerateNumberActivity.kt
@@ -0,0 +1,35 @@
+package edu.ntnu.oflarsen.idatt2506.oving2
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+
+class GenerateNumberActivity : Activity() {
+    private var upperLimit = 100
+    private var randomNumber = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_number)
+        upperLimit = intent.getIntExtra("upperLimit", upperLimit)
+    }
+
+    /**
+     *  Avslutt aktivitet og gå tilbake til MainActivity, legg ved intent med riktig flagg
+     */
+    fun onClickFinishActivity(v: View?) {
+
+        setResult(RESULT_OK, Intent().putExtra("numberValue", randomNumber))
+        finish()
+    }
+
+    fun generateRandomNumber(view: View?){
+        randomNumber = (0..upperLimit).random()
+
+        setResult(RESULT_OK, Intent().putExtra("numberValue", randomNumber))
+        finish()
+    }
+
+}
--- Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
@@ -1,21 +1,38 @@
 package edu.ntnu.oflarsen.idatt2506.oving2
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
-
+    private val numbRequestCode: Int = 0
+    private val upperLimit = 100;
+    private var value = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun generateRandomNumber(view: View?){
-        val value = (0..100).random()
+    fun onClickStartApp(v: View?) {
+        val intent = Intent("oflarsen.idatt2506.GenerateNumberActivity")
+        intent.putExtra("upperLimit", upperLimit)
+        startActivityForResult(intent, value)
+    }
 
-        Toast.makeText(this, value.toString(), Toast.LENGTH_LONG).show()
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (resultCode != RESULT_OK) {
+            Log.e("onActivityResult()", "Something went wrong")
+            return
+        }
+
+        if (requestCode == numbRequestCode) {
+            value = data.getIntExtra("numberValue", value)
+            val numberValue = findViewById<View>(R.id.numbValue) as TextView
+            numberValue.text = value.toString()
+        }
     }
 }
