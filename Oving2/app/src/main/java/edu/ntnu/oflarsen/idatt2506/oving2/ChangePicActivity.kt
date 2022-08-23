c6616bd5203fb3fe839e6cf3e55f2fd8857ac5ec
ofplarsen <ofplarsen@gmail.com>
2022-08-23 10:13:25 +0200

    Chapter 2.1-2.3 done

--- Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/ChangePicActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/ChangePicActivity.kt
@@ -1,19 +1,27 @@
 package edu.ntnu.oflarsen.idatt2506.oving2
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 
 class ChangePicActivity : Activity() {
-    var flagValue = R.drawable.lukas;
+    private var flagValue = R.drawable.lukas;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_pic)
+
+        flagValue = intent.getIntExtra("flag", flagValue)
+        val image = findViewById<View>(R.id.imageView) as ImageView
+        image.setImageResource(flagValue)
     }
 
-    fun onClickFinishActivity(v: View?) { finish() }
+    fun onClickFinishActivity(v: View?) {
+        setResult(RESULT_OK, Intent().putExtra("flag", flagValue))
+        finish()
+    }
 
     fun onClickChangePic(v: View?) {
         val currentImageIsUK: Boolean = flagValue == R.drawable.lukas
--- Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/MainActivity.kt
@@ -3,9 +3,13 @@ package edu.ntnu.oflarsen.idatt2506.oving2
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 
 class MainActivity : Activity() {
+    private val flagRequestCode: Int = 1
+    private var flagValue = R.drawable.steffen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,6 +17,23 @@ class MainActivity : Activity() {
 
     fun onClickStartChangePicActivity(v: View?) {
         val intent = Intent("oving2.ChangePicActivity")
-        startActivity(intent) }
+        intent.putExtra("flag", flagValue)
+        startActivityForResult(intent, flagRequestCode)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_OK) {
+            Log.e("onActivityResult()", "Noe gikk galt")
+            return
+        }
+        if (requestCode == flagRequestCode) {
+            if (data != null) {
+                flagValue = data.getIntExtra("flag", flagValue)
+            }
+            val flag = findViewById<View>(R.id.imageView) as ImageView
+            flag.setImageResource(flagValue)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
