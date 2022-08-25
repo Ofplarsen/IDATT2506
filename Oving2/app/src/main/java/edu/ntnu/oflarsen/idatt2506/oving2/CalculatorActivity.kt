3a8c4e23095cc27c42ee999778f9b4ec048296a9
ofplarsen <ofplarsen@gmail.com>
2022-08-25 12:51:30 +0200

    Simple logic in calc works

--- Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/CalculatorActivity.kt
+++ Oving2/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving2/CalculatorActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -22,5 +23,35 @@ class CalculatorActivity : Activity() {
 
         setResult(RESULT_OK, Intent().putExtra("numberValue", 2))
         finish()
+    }
+
+    fun onClickMultiply(v: View?){
+        val n1 = findViewById<View>(R.id.n1) as TextView
+        val n2 = findViewById<View>(R.id.n2) as TextView
+        val a = findViewById<View>(R.id.answerInput) as EditText
+
+        val answerUser = a.text.toString().toInt()
+        val answer = n1.text.toString().toInt() * n2.text.toString().toInt()
+
+        showToast(answer == answerUser, answer)
+    }
+
+    fun onClickAdd(v: View?){
+        val n1 = findViewById<View>(R.id.n1) as TextView
+        val n2 = findViewById<View>(R.id.n2) as TextView
+        val a = findViewById<View>(R.id.answerInput) as EditText
+
+        val answerUser = a.text.toString().toInt()
+        val answer = n1.text.toString().toInt() + n2.text.toString().toInt()
+
+        showToast(answer == answerUser, answer)
+    }
+
+    private fun showToast(correct: Boolean, answer: Int){
+        if(correct){
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this, getString(R.string.wrong) + answer.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
