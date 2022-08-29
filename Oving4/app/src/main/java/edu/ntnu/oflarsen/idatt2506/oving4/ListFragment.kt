36275f92f66dc7316941e1592de9f34a1fb552ae
ofplarsen <ofplarsen@gmail.com>
2022-08-29 13:00:29 +0200

    Some handling of fragment orentation

---
+++ Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/ListFragment.kt
@@ -0,0 +1,6 @@
+package edu.ntnu.oflarsen.idatt2506.oving4
+
+import androidx.fragment.app.Fragment
+
+class ListFragment : Fragment() {
+}
--- Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/MainActivity.kt
+++ Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/MainActivity.kt
@@ -1,11 +1,26 @@
 package edu.ntnu.oflarsen.idatt2506.oving4
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        setOrientation(newConfig)
+    }
+
+    private fun setOrientation(config: Configuration) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val isPortrait: Boolean = config.orientation == Configuration.ORIENTATION_PORTRAIT
+        transaction.replace(R.id.content, (if (isPortrait) ListFragment() else InfoFragment()))
+        transaction.commit()
     }
 }
