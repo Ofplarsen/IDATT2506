8a4f27c945e412682eff943b7ebbeab2c1586388
ofplarsen <ofplarsen@gmail.com>
2022-08-29 11:13:09 +0200

    Added two fragments with split view

---
+++ Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/Fragment1.kt
@@ -0,0 +1,14 @@
+package edu.ntnu.oflarsen.idatt2506.oving4
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+}
