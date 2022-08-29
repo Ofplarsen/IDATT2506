4b7c678ff6eefecf545208eaf7ee96a24c75331d
ofplarsen <ofplarsen@gmail.com>
2022-08-29 11:29:37 +0200

    Handling phone orentation changes

--- Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/MainActivity.kt
+++ Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/MainActivity.kt
@@ -1,11 +1,27 @@
 package edu.ntnu.oflarsen.idatt2506.oving4
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.example)
+        //setOrientation(resources.configuration)
+    }
+
+    private fun setOrientation(config: Configuration) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        val isPortrait: Boolean = config.orientation == Configuration.ORIENTATION_PORTRAIT
+        transaction.replace(R.id.content, if (isPortrait) Fragment1() else Fragment2())
+        transaction.commit()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        setOrientation(newConfig)
     }
 }
