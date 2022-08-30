9ac37d2967a557f9220e130401b7928102f5a4a5
ofplarsen <ofplarsen@gmail.com>
2022-08-30 13:19:06 +0200

    Simple handling for orentation change

--- Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/MainActivity.kt
+++ Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/MainActivity.kt
@@ -3,12 +3,13 @@ package edu.ntnu.oflarsen.idatt2506.oving4
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_horizontal)
+        setOrientation(resources.configuration)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -17,9 +18,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setOrientation(config: Configuration) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        val isPortrait: Boolean = config.orientation == Configuration.ORIENTATION_PORTRAIT
-        transaction.replace(R.id.content, (if (isPortrait) ListFragment() else InfoFragment()))
-        transaction.commit()
+        val isPortrait = config.orientation == Configuration.ORIENTATION_PORTRAIT
+
+        if (isPortrait)
+            setContentView(R.layout.activity_main_vertical); // it will use .xml from /res/layout
+        else
+            setContentView(R.layout.activity_main_horizontal); // it will use xml from /res/layout-land
+
     }
 }
