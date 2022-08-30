e78b31eb278d5cdd58de3465dbf884045677d370
ofplarsen <ofplarsen@gmail.com>
2022-08-30 13:56:00 +0200

    Layout done

--- Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/ListFragment.kt
+++ Oving4/app/src/main/java/edu/ntnu/oflarsen/idatt2506/oving4/ListFragment.kt
@@ -1,16 +1,30 @@
 package edu.ntnu.oflarsen.idatt2506.oving4
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.fragment.app.ListFragment
 
-class ListFragment : Fragment() {
+class ListFragment  : ListFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.list_fragment, container, false);
+    private var movies: Array<String> = arrayOf()
+    private var descriptions: Array<String> = arrayOf()
+    private var clicked = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        movies = resources.getStringArray(R.array.movies)
+        descriptions = resources.getStringArray(R.array.movie_descs)
+        listAdapter = activity?.let {
+            ArrayAdapter(it, android.R.layout.simple_list_item_1, android.R.id.text1, movies)
+        }
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+        Toast.makeText(context, movies[position], Toast.LENGTH_SHORT).show()
+        clicked = position
     }
 }
